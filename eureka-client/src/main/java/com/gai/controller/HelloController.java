d0fc24d59b4477fc93a0936b35593a81e0cca4b7
gaidongxu <gaidongxu@dataexa.com>
2021-01-15 19:28:00 +0800

    Initial commit

---
+++ eureka-client/src/main/java/com/gai/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.gai.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @GetMapping("/sayHello")
+    public String sayHello(String name){
+        String result = "sayHello to " + name;
+        List<ServiceInstance> instanceList = client.getInstances(serviceId.toUpperCase());
+        if(instanceList != null && instanceList.size() != 0){
+            ServiceInstance instance = instanceList.get(0);
+            String host = " host: " + instance.getHost();
+            String uri = " uri: " + instance.getUri();
+            String port = " port: " + instance.getPort();
+            String serviceId = " serviceId: " + instance.getServiceId();
+            result = result + host + uri + port + serviceId;
+        }
+        return result;
+    }
+}
---
+++ feign-consumer/src/main/java/com/gai/FeignConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FeignConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignConsumerApplication.class,args);
+    }
+}
